f835f9d40a98903c0a81e769f6a3a295a4645636
rafaatrevisan <rafa_tre2@hotmail.com>
2025-03-16 14:07:56 -0300

    first commit

---
+++ src/main/java/com/helpbus/HelpBus/HelpBusApplication.java
@@ -0,0 +1,13 @@
+package com.helpbus.HelpBus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelpBusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelpBusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/helpbus/HelpBus/HelpBusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.helpbus.HelpBus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelpBusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
