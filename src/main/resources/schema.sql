f75cdfdb38a98c1c954cf6c5f1d593b5a096fdb5
rafaatrevisan <rafa_tre2@hotmail.com>
2025-09-05 00:55:56 -0300

    Mudança para H2 Database

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,46 @@
+CREATE TABLE cargos (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  nome VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE pessoa (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  nome VARCHAR(255),
+  data_nascimento DATE,
+  telefone VARCHAR(11),
+  id_cargo INT NOT NULL,
+  CONSTRAINT fk_pessoa_cargo FOREIGN KEY (id_cargo) REFERENCES cargos(id)
+);
+
+CREATE TABLE ticket (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  data_ticket TIMESTAMP NOT NULL,
+  id_usuario INT NOT NULL,
+  status INT,
+  prioridade INT,
+  andar VARCHAR(255),
+  localizacao VARCHAR(255),
+  descricao_localizacao VARCHAR(255),
+  descricao_ticket_usuario VARCHAR(255),
+  imagem VARCHAR(255),
+  CONSTRAINT fk_ticket_usuario FOREIGN KEY (id_usuario) REFERENCES pessoa(id)
+);
+
+CREATE TABLE resolucao_ticket (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  id_ticket INT NOT NULL,
+  descricao VARCHAR(255) NOT NULL,
+  data_resolucao TIMESTAMP NOT NULL,
+  id_funcionario INT NOT NULL,
+  CONSTRAINT fk_resolucao_ticket_ticket FOREIGN KEY (id_ticket) REFERENCES ticket(id),
+  CONSTRAINT fk_resolucao_ticket_funcionario FOREIGN KEY (id_funcionario) REFERENCES pessoa(id)
+);
+
+CREATE TABLE usuario (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  id_pessoa INT,
+  email VARCHAR(255) UNIQUE,
+  senha VARCHAR(255),
+  data_cadastro TIMESTAMP NOT NULL,
+  CONSTRAINT fk_usuario_pessoa FOREIGN KEY (id_pessoa) REFERENCES pessoa(id)
+);
