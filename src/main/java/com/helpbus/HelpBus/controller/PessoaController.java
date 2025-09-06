daaf32ed6ba008184e5cf356795110bbe2b7355e
Rafael Trevisan <rafa_tre2@hotmail.com>
2025-09-05 21:52:23 -0300

    Criação das entidades Ticket e ResolucaoTicket, e dos ENUMs StatusTicket e PrioridadeTicket

--- src/main/java/com/helpbus/HelpBus/controller/PessoaController.java
+++ src/main/java/com/helpbus/HelpBus/controller/PessoaController.java
@@ -14,7 +14,7 @@ import org.springframework.web.server.ResponseStatusException;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/helpbus/pessoas")
+@RequestMapping("/api/fixhub/pessoas")
 @RequiredArgsConstructor
 public class PessoaController {
 
---
+++ src/main/java/com/helpbus/HelpBus/model/enums/PrioridadeTicket.java
@@ -0,0 +1,8 @@
+package com.helpbus.HelpBus.model.enums;
+
+public enum PrioridadeTicket {
+    BAIXA,
+    REGULAR,
+    IMPORTANTE,
+    URGENTE
+}
