3d35b91b09d2dc1322ed504ecc908f70e41d7b1a
rafaatrevisan <rafa_tre2@hotmail.com>
2025-11-12 02:23:31 -0300

    Correção no exportar CSV.

--- src/main/java/com/fixhub/FixHub/service/RelatorioService.java
+++ src/main/java/com/fixhub/FixHub/service/RelatorioService.java
@@ -160,7 +160,7 @@ public class RelatorioService {
             baos.write(0xBF);
 
             // Cabeçalho
-            writer.println("ID;Descrição;Cliente;Status;Prioridade;Equipe;Funcionário;Data Criação;Data Resolução;Tempo (min)");
+            writer.println("ID;Descrição;Status;Prioridade;Equipe;Funcionário;Data Criação;Data Resolução;Tempo (min)");
 
             // Dados
             for (RelatorioTicketsDTO ticket : tickets) {
